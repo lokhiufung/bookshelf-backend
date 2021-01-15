e6fc60b3c67b6498b784e4511623d6486801f28e
lokhiufung <lokhiufung123@gmail.com>
2021-01-15 16:09:30 +0800

    added Dockerfile

--- python/app/db/mongo_utils.py
+++ python/app/db/mongo_utils.py
@@ -1,9 +1,15 @@
+import os
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from .mongodb import db
 
+DB_HOST = os.environ.get('DB_HOST', '0.0.0.0')
+DB_PORT = os.environ.get('DB_PORT', 27017)
+
+
 async def connect_to_mongo():
-    db.client = AsyncIOMotorClient()
+    db.client = AsyncIOMotorClient(host=DB_HOST, port=DB_PORT)
 
 
 async def close_mongo_connection():
