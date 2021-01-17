fb34aea726404bd65d7c736afb16abdb2fc424a5
lokhiufung <lokhiufung123@gmail.com>
2021-01-18 01:18:35 +0800

    Update seeder.py

--- python/seeder.py
+++ python/seeder.py
@@ -4,9 +4,14 @@ from app import config
 
 
 def main():
+    # initialize a database
+
+    db_name = config.DATABASE_NAME
+    col_name = config.BOOK_COLLECTION
+    
     client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client[config.DATABASE_NAME]
-    col= db[config.BOOK_COLLECTION]
+    db = client[db_name]
+    col= db[col_name]
 
     col.insert_one({
         'title': '<seeder-book>',
@@ -15,6 +20,18 @@ def main():
         'tags': [],
         'book_id': "0000"
     })
+    client.close()
+    print(f'created a database {db_name} and a collection {col_name}')
+    
+    # remove seeder doc from db
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client[db_name]
+    col= db[col_name]
+    col.delete_many({})  # remove all docs
+    client.close()
+    print(f'removed all seeder docs from the database {db_name} and collection {col_name}')
+
+    
 
 if __name__ == '__main__':
     main()
